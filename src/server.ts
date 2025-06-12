5c1add6e43972a374803cd66c989f266f5af7b09
Diego Longarai Furlan <longarai.furlan@gmail.com>
2025-06-12 12:35:22 +0100

    Create Server

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,14 @@
-console.log("Hello world!")
+import * as http from "http"
+
+const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse)=> {})
+
+
+
+const port = process.env.PORT
+
+server.listen(port, ()=> {
+    console.log(`servidor iniciado na porta ${port}`)
+})
+
+
 

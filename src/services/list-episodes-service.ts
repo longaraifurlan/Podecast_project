8e03717d4007179db401cef8b1aa4d08b6431de8
Diego Longarai Furlan <longarai.furlan@gmail.com>
2025-06-16 11:31:16 +0100

    Repo/Models

--- src/services/list-episodes-service.ts
+++ src/services/list-episodes-service.ts
@@ -1,21 +1,9 @@
+import { repositoryPodcast } from "../repositories/podcasts-repository"
 
 
 
 export const servceListEpisodes = async () => {
-    const data = [
-        {
-    podcastName: "Flow",
-    episode: "CBUM - Flow #319",
-    videoId: "pQSuQmUfS30&t=5s",
-    categories: ["saúde", "esporte", "bodybuilder"]
-},
-{
-    podcastName: "Flow",
-    episode: "RUBENS BARRICHELLO - Flow #339",
-    videoId: "4KDGTdiOV4I",
-    categories: ["esporte", "corrida"]
-}
-    ]
+    const data = await repositoryPodcast()
 
     return data
 }
